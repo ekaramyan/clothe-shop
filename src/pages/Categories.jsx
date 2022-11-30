eef0bc9be9b3ef8ee45fc0faa338881b1ac3bdea
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2022-11-30 22:28:23 +0400

    fixing cards

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -56,14 +56,17 @@ export default class Categories extends Component {
               text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
               style={{ width: '18rem' }}
               className="mb-2"
+              // img={variant}
             >
-              <Card.Header>Header</Card.Header>
+              
               <Card.Body>
                 <Card.Title>{variant} Card Title </Card.Title>
+                <Card.Img src=''/>
                 <Card.Text>
                   Some quick example text to build on the card title and make up the
                   bulk of the card's content.
                 </Card.Text>
+                <Card.Header>Header</Card.Header>
               </Card.Body>
             </Card>
           ))}
