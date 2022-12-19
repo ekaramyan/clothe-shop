067d07a553efb9110c2f0f5d6923a0c711de2a1a
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2022-12-20 02:59:34 +0400

    finished categories with fake json

--- src/pages/Female.jsx
+++ src/pages/Female.jsx
@@ -2,10 +2,27 @@ import React, { Component } from 'react'
 import Cards from '../components/Card'
 
 const Female = (props) => {
+  useEffect(() => {
+    fetchData()
+  }, []
+  )
+
+  let [products, setProducts] = useState([{}])
+
+  async function fetchData() {
+    const productsShow = await productsAPI.getAll();
+    setProducts(productsShow)
+  }
+
   return (
-    <div>
-      fdofhv
-    </div >
+    <div className='cards__wrapper'>
+
+      {products.length !== 0
+        ? <ClotheList products={products} title={'Женская одежда'} />
+        : <p className='not-found'>No products found</p>
+      }
+
+    </div>
   )
 }
 
